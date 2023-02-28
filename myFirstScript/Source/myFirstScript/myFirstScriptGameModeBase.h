c9734248993835a3c0fd2afd86de45e5dd48238e
Alejandro Velez <alex@alexv24.com>
2023-02-27 23:59:35 -0700

    first commit

---
+++ myFirstScript/Source/myFirstScript/myFirstScriptGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "myFirstScriptGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFIRSTSCRIPT_API AmyFirstScriptGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
